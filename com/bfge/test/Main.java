6d892c51279892b7809d0fe8e91201d5e30966a5
bfge <2426030618@qq.com>
2025-06-27 12:33:16 +0800

    初始化提交！！

---
+++ com/bfge/test/Main.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: Main
+ * Description:
+ *
+ * @Author bfge
+ * @Create 2025/6/27 12:32
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("初始化提交！！！！");
+    }
+}
